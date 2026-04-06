b93fe22aa7022feae308e0d2819cb47343da1ed0
daorellanam <daorellanamgt@gmail.com>
2026-04-06 11:57:21 -0600

    Fix: replace figma:asset imports with local asset paths in PortalArchivoPage.tsx

--- src/app/components/portfolio/PortalArchivoPage.tsx
+++ src/app/components/portfolio/PortalArchivoPage.tsx
@@ -1,4 +1,4 @@
-import imgMockup from "figma:asset/b359b2013cac8fea9fc63e0d9ee92216f32612d6.png";
+import imgMockup from "../../../assets/b359b2013cac8fea9fc63e0d9ee92216f32612d6.png";
 import videoMockup from "../../../imports/test.mp4";
 import { CaseStudyLayout } from "./CaseStudyLayout";
 
